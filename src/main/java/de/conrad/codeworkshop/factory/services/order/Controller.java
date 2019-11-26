83732be66044413c1591d121a5ccf7cc62b0b23c
Andreas Hartmann <andreas.hartmann@conrad.de>
2019-11-26 16:06:36 +0100

    JavaDoc improvements.

--- src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
+++ src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
- *
  * @author Andreas Hartmann
  */
 @RestController("orderController")
